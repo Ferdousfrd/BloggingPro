4009c1e80e12681574a3673dc91126cff6eb8643
Ferdousfrd <ferdous.frd81@gmail.com>
2024-04-13 01:45:55 +0300

    login settings done but having issues with logout

--- users/views.py
+++ users/views.py
@@ -11,8 +11,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('home_view')
+            messages.success(request, f'Your account has been created. Log in Please!')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {
