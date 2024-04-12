72439703a12bf78818458dc973bc35a24372c257
Ferdousfrd <ferdous.frd81@gmail.com>
2024-04-12 19:13:06 +0300

    Added registration app and created form for user sign up

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author'        : 'CoreyMS',
-        'title'         : 'Blog Post 1',
-        'content'       : 'First Post Content',
-        'date_posted'   : 'March 27, 2024'
-    },
-    {
-        'author'        : 'Jane Doe',
-        'title'         : 'Blog Post 2',
-        'content'       : 'Second Post Content',
-        'date_posted'   : 'March 27, 2024'
-    }
-]
 
 # Create your views here.
 def home_view(request):
     return render(request, "blog/home_view.html", {
-        "post_content" : posts,
+        "post_content" : Post.objects.all(),
         "title" : "Blogs"
     })
 
--- personal_project1/settings.py
+++ personal_project1/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog',
+    'users',
+    'crispy_forms',
+    'crispy_bootstrap5', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
