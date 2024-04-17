a4c408db6542214ec461d6b7e399aa0e987c4313
Ferdousfrd <ferdous.frd81@gmail.com>
2024-04-17 04:29:20 +0300

    added user profile funtion and photo uploading and saving through Media. Also learned about signal and now new user can create and save their profile when register

--- personal_project1/urls.py
+++ personal_project1/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,6 @@ urlpatterns = [
     path('', include("blog.urls")),
     path('', include("users.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
