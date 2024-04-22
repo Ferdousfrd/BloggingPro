40c0131cca33882734c873839bc5b013af87e43d
Ferdousfrd <ferdous.frd81@gmail.com>
2024-04-22 21:45:21 +0300

    Added post update, delete features and added those urls and buttons too

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 
 urlpatterns = [
     path("", PostListView.as_view(), name="home_view"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/new/", PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/update", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post_delete"),
     path("about/", views.about_view, name="about_view")
+    
 ]
